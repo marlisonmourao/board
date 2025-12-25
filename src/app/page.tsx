3c97a872a3be6b95c9cf5967c67131aa27fffc9a
marlisonmourao <bentesmourao@gmail.com>
2025-12-24 20:04:27 -0400

    feat: Implement initial board UI with new components, dependencies, and biome configuration adjustment.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,48 @@
+import { Button } from "@/components/button"
+import { Card } from "@/components/card"
+import { Section } from "@/components/section"
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react"
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
-    </main>
+    <div className="max-w-[1620px] w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div></div>
+
+      <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+        <Section.Root>
+          <Section.Header>
+            <Section.Title>
+              <ArchiveIcon className="size-3" />
+              Backlog
+            </Section.Title>
+
+            <Section.IssueCount>20</Section.IssueCount>
+          </Section.Header>
+
+          <Section.Content>
+            <Card.Root>
+              <Card.Header>
+                <Card.Number>ECO-001</Card.Number>
+                <Card.Title>Implementar cartão de crédito</Card.Title>
+              </Card.Header>
+
+              <Card.Footer>
+                <Button>
+                  <ThumbsUpIcon className="size-3" />
+
+                  <span className="text-sm">12</span>
+                </Button>
+
+                <Button>
+                  <MessageCircleIcon className="size-3" />
+
+                  <span className="text-sm">6</span>
+                </Button>
+              </Card.Footer>
+            </Card.Root>
+          </Section.Content>
+        </Section.Root>
+      </main>
+    </div>
   )
 }
